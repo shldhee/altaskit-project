9e50bf75da8b6e2f79af2dff4db30d60de0bbc4f
Duckhee Lee <shldhee@yahoo.co.kr>
2025-01-17 20:13:10 +0900

    feature: 프로젝트 세팅
    - react, webpack, babel, typescript 추가 완료

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+// webpack.config.js
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const { CompiledExtractPlugin } = require("@compiled/webpack-loader");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js", // 생성될 번들 파일 이름
+    path: path.resolve(__dirname, "dist"), // 생성 파일이 저장될 디렉토리
+    publicPath: "/", // 브라우저에서 번들 파일이 제공될 경로
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: [
+          { loader: "babel-loader" },
+          {
+            // ↓↓ Compiled should run last ↓↓
+            loader: "@compiled/webpack-loader",
+            options: {
+              transformerBabelPlugins: ["@atlaskit/tokens/babel-plugin"],
+              extract: true,
+              inlineCss: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /compiled-css\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /(?<!compiled-css)(?<!\.compiled)\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CompiledExtractPlugin({ sortShorthand: true }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"), // 정적 파일 경로
+    historyApiFallback: true, // 모든 경로를 index.html로 리다이렉트
+    hot: true, // HMR 활성화
+    port: 3000, // 포트 번호
+  },
+};
