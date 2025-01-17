b67c63c4f09c95e4a61a955d4a5a230d2022796f
Duckhee Lee <shldhee@yahoo.co.kr>
2025-01-17 20:18:25 +0900

    feature: 라우터 작업
    - react-router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import Home from "@/pages";
+import UserList from "@/pages/users";
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>My React and TypeScript App!</h1>
-    </div>
+    <>
+      <div>
+        <h1>My React and TypeScript App!</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<UserList />} />
+      </Routes>
+    </>
   );
 };
 
