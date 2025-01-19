91ed66fa469882d33213f5299fec50679ec8fedf
Duckhee Lee <shldhee@yahoo.co.kr>
2025-01-19 21:29:38 +0900

    feature: 게시물 제목 검색 기능 작업

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
 import { PER_PAGE } from "@/constants/constants";
 import { usePagination } from "@/hooks/usePagination";
 import { usePostsQuery } from "@/services/post/usePostsQuery";
 import { formatISODate } from "@/utils/date";
+import { Label } from "@atlaskit/form";
 import Pagination from "@atlaskit/pagination";
 import SectionMessage from "@atlaskit/section-message";
 import Spinner from "@atlaskit/spinner";
@@ -12,15 +15,36 @@ import TableTree, {
   Row,
   Rows,
 } from "@atlaskit/table-tree";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 const PostList = () => {
   const { t } = useTranslation();
   const { data, isLoading, isError } = usePostsQuery();
+
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [filteredData, setFilteredData] = useState(data || []);
+
+  useEffect(() => {
+    setFilteredData(data || []);
+  }, [data]);
+
+  const handleSearch = () => {
+    setFilteredData(
+      searchKeyword.trim() === "" ? getAllData() : getFilteredData()
+    );
+  };
+
+  const getAllData = () => data || [];
+
+  const getFilteredData = () =>
+    (data || []).filter((post) =>
+      post.title.toLowerCase().includes(searchKeyword.toLowerCase())
+    );
+
   const { setCurrentPage, paginatedData, totalPages } = usePagination({
-    data: data || [],
+    data: filteredData || [],
     itemsPerPage: PER_PAGE,
   });
 
@@ -53,6 +77,18 @@ const PostList = () => {
   return (
     <div>
       <h2 className="text-2xl font-semibold mb-4">{t("Post-List")}</h2>
+
+      <div className="w-4/5 max-w-80 mb-4">
+        <Label htmlFor={"title"}>제목</Label>
+        <Input
+          id="title"
+          placeholder={t("Search by title")}
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.currentTarget.value)}
+        />
+      </div>
+      <Button onClick={handleSearch}>검색</Button>
+
       <TableTree>
         <Headers>
           <Header width={100}>{t("post.id")}</Header>
